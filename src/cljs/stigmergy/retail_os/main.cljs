062f78bc0e2b0dbeefd656be2f87abef22e81c74
sto <son.c.to@gmail.com>
2019-08-16 16:11:57 -0400

    flex-box within css-grid

--- src/cljs/stigmergy/retail_os/main.cljs
+++ src/cljs/stigmergy/retail_os/main.cljs
@@ -4,23 +4,27 @@
 (def state (r/atom {:txt "hello"}))
 
 (defn grid [state]
-  (let [grid-container {:display :grid
-                        :grid-template-columns "auto minmax(200px,auto)"
-                        :background-color "#2196F3"
-                        :padding 10}
+  (let [grid {:display :grid
+              :grid-template-columns "auto minmax(200px,auto)"
+              :background-color "#2196F3"
+              :padding 10}
         grid-item {:background-color "rgba(255, 255, 255, 0.8)"
                    :border "1px solid rgba(0, 0, 0, 0.8)"
                    :padding 5
                    :font-size 30
-                   :text-align :center}]
-    [:div {:style grid-container}
+                   :text-align :center}
+        flexbox {:display :flex
+                 :background-color :green
+                 :flex-direction :row
+                 :flex-wrap :wrap
+                 :justify-content :flex-start}]
+    [:div {:style grid}
+     [:div {:style flexbox}
+      (for [i (range 126)]
+        ^{:key i} [:button {:style {:flex-grow 1
+                                    :width 150}} i])]
      [:div {:style grid-item}
-      [:div 
-       (for [i (range 1000)]
-         ^{:key i} [:button i])]
-      ]
-     [:div {:style grid-item}
-      [:button.button 2]]]))
+      [:button 2]]]))
 
 (defn init []
   (r/render-component [grid state] (js/document.getElementById "app"))
